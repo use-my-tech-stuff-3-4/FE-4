14eaaf692f01c58f5fcb230e99250096344a88a2
Christine Smith <christinesm@google.com>
2020-03-01 14:19:14 -0800

    redid the registration form and added a working POST request

--- rentech/src/index.js
+++ rentech/src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { userReducer } from './reducers/userReducer';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(userReducer, applyMiddleware(thunk));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
